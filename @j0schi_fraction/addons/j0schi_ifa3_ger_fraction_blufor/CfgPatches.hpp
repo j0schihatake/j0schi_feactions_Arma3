a3313d3f7a2d63b8771ba18035cdd7b2f0772263
j0schitadamori <j0schitadamori@gmail.com>
2024-11-29 09:52:52 +0300

    Убрал у солдат оружие ближнего боя.

---
+++ @j0schi_fraction/addons/j0schi_ifa3_ger_fraction_blufor/CfgPatches.hpp
@@ -0,0 +1,16 @@
+class CfgPatches {
+    class joschi_ifa3_ger_fraction {
+        units[] = {
+            "joschi_ifa3_ger_medic",
+            "joschi_ifa3_ger_assault",
+            "joschi_ifa3_ger_sniper",
+            "joschi_ifa3_ger_gunner",
+            "joschi_ifa3_ger_pt"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {};
+        author = "j0sch";
+        authors[] = {"j0sch"};
+    };
+};
---
+++ @j0schi_fraction/addons/j0schi_ifa3_rus_fraction_opfor/CfgPatches.hpp
@@ -0,0 +1,17 @@
+class CfgPatches {
+    class joschi_ifa3_rus_fraction {
+        units[] = {
+            "joschi_ifa3_rus_medic",
+            "joschi_ifa3_rus_assault",
+            "joschi_ifa3_rus_sniper",
+            "joschi_ifa3_rus_gunner",
+            "joschi_ifa3_rus_pt_a",
+            "joschi_ifa3_rus_pt_b"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {};
+        author = "j0sch";
+        authors[] = {"j0sch"};
+    };
+};
